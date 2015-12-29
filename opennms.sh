df92914b94a0722b37af25c1ee352946ba75d9a1
naveenlj <naveen09mca@gmail.com>
2015-12-30 00:08:15 +0530

    Update opennms.sh

--- opennms.sh
+++ opennms.sh
@@ -1,15 +1,16 @@
 #/bin/bash
 
- rpm -Uvh https://yum.opennms.org/repofiles/opennms-repo-RELEASE-DISTRIBUTION.noarch.rpm
+rpm -Uvh https://yum.opennms.org/repofiles/opennms-repo-RELEASE-DISTRIBUTION.noarch.rpm
  
-  yum install postgresql postgresql-server
+yum install postgresql postgresql-server
   
-   /sbin/service postgresql start
-   
-    /sbin/service postgresql initdb
- /sbin/service postgresql start
+/sbin/service postgresql start
  
-  /sbin/chkconfig postgresql on
+/sbin/service postgresql initdb
+
+/sbin/service postgresql start
+ 
+/sbin/chkconfig postgresql on
 
 # Need to change from ident to trust on  pg_hba.conf file   
 #local   all         all                               trust
@@ -17,3 +18,22 @@
 #host    all         all         ::1/128               trust
 
  /sbin/service postgresql restart
+
+yum install java-1.7.0-openjdk-devel
+
+ yum -y install opennms
+
+/opt/opennms/bin/runjava -S /usr/java/latest/bin/java
+
+/opt/opennms/bin/install -dis
+
+yum -y install iplike
+
+firewall-cmd --permanent --add-port=8980/tcp
+firewall-cmd --reload
+
+systemctl start opennms
+systemctl enable opennms
+
+
+
